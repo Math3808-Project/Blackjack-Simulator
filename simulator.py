3085771b3a6ad42217bfe6d03c8746ffed91af13
cailynse <cailyn.edwards@shopify.com>
2021-04-21 16:29:42 -0400

    add concurrency and comments

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@
 # Runs multiple simulations of the game and saves the results
 
 import game
-import multiprocessing as mp
+import concurrent.futures
 import csv
 
 class Simulator:
@@ -10,44 +10,103 @@ class Simulator:
     
     Parameters:
 
-    sim_count (int): Indicated the number of simulations to compute
-
+    sim_count (int): Indicates the number of simulations to compute - will be rounded up to the
+    
+    nearest multiple of 10000 
+    
+    bet (int): Represents the starting bet for the Black Jack game
     """
 
     def __init__(self, sim_count=1000000, bet_amount=1):
-        self.sim_count = sim_count
-        self.bet_amount = bet_amount
+        if sim_count % 10000 != 0: 
+            self.sim_count = sim_count + 10000 - (sim_count % 10000)
+        else:
+            self.sim_count = sim_count
 
-    def main(self):
+        self.bet_amount = bet_amount
+        self.completed_sims = 0
+
+    def start(self):
+        """ Runs the Simulation with the initialized attributes.
+
+        Outputs:
+        
+        The result of the current simulation as well as the overall result of all simulations run previously.
+
+        Appends the most recent result to results.csv
+        
+        Updates overall_avg.csv with the updated totals for total_sims and result_sum
+        """
         print(f'Starting Blackjack Simulator with {self.sim_count} simulations')
         results = []
-        
-        #TODO: Implement concurrency
-        for x in range(self.sim_count):
-            results.append(self.run_game())
+
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            p_list = []
+            for i in range(int(self.sim_count/10000)):
+                p_list.append(executor.submit(self.run_multiple_games, 10000))
+                
+                for res in concurrent.futures.as_completed(p_list):
+                    results.append(res.result())
+                
         
         self.write_results(results)
         
-        print(f'Result for this simulation: {sum(results)/self.sim_count}')
+        print(f'Result for this simulation batch: {sum(results)/self.sim_count}')
         print(f'Adjusted overall result: {self.get_overall_average(results)}')
 
-        # for x in range(10):
-        #     p = Process(target=run_game)
-        #     p.start()
-        #     p.join()
 
+    def run_single_game(self):
+        """ Runs a single Black Jack game.
 
-    def run_game(self):
+        Returns:
+        
+        int: the result of the game
+        """
+
         bj = game.Game()
         result_dict = bj.game_result(bet=self.bet_amount)
         return result_dict["result"]
+    
+    def run_multiple_games(self, num_games):
+        """ Runs a given number of Black Jack games.
+
+        Returns:
+        
+        int: the result sum of the games
+        """
+        results = []
+        for x in range(num_games):
+            results.append(self.run_single_game())
+        self.completed_sims += num_games
+        print(f'Simulations completed so far: {self.completed_sims}')
+        return sum(results)
 
     def write_results(self, results):
+        """ Writes the results of all simulations to results.csv.
+
+        Parameters:
+        
+        arr[int]: an array of game results
+        """
+
         res = open("results.csv", "a")
         res.write(f'{self.sim_count},{self.bet_amount},{sum(results)}\n')
         res.close()
     
     def get_overall_average(self, results):
+        """ Calulates the overall average of results from most recent and historical simulations and
+            updates the values in overall_avg.csv
+     
+
+        Parameters:
+        
+        arr: an array of game results
+
+        Returns:
+        
+        float: the average of all results
+        """
+
         total_sims = self.sim_count
         result_sum = sum(results)
         with open('overall_avg.csv', newline='') as csvfile:
@@ -61,4 +120,4 @@ class Simulator:
         return result_sum/total_sims
 
 if __name__ == "__main__":
-    Simulator().main()
+    Simulator().start()
